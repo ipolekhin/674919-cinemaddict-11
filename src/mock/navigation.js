85d90940cc0cf0e3dba283c09738e5894ac550f5
ipolekhin <ipolekhin@yadnex.ru>
2020-04-18 12:27:11 +0300

    3.1.4 Считаем статистику navigation

--- src/mock/navigation.js
+++ src/mock/navigation.js
@@ -1,17 +1,35 @@
 import {NAVIGATION_NAMES} from "../const";
 
-// Функция считатет статиститку фильтра
-// const calculateStatistics = (films) => {
-//   return films.reduce(())
-// };
+// Функция считатет статиститку навигации
+const calculateStatistics = (films) => {
+  return films.reduce((result, {isWatchlist, isWatched, isFavorite}) => {
+    result[`All movies`]++;
+    if (isWatchlist) {
+      result[`Watchlist`]++;
+    }
+    if (isWatched) {
+      result[`History`]++;
+    }
+    if (isFavorite) {
+      result[`Favorites`]++;
+    }
+
+    return result;
+  }, {
+    'All movies': 0,
+    'Watchlist': 0,
+    'History': 0,
+    'Favorites': 0,
+  });
+};
 
 const generateNavigations = (films) => {
-  // const statistics = calculateStatistics(films);
+  const statistics = calculateStatistics(films);
 
   return NAVIGATION_NAMES.map((name) => {
     return {
       'name': name,
-      'count': 2,
+      'count': statistics[name],
     };
   });
 };

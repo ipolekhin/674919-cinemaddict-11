2828ae7dde06d2e06d7519992b0158b2f7b3a4b6
ipolekhin <ipolekhin@yadnex.ru>
2020-05-15 13:46:26 +0300

    6.1.1 Создадим контроллер фильма.

---
+++ src/controllers/movie.js
@@ -0,0 +1,14 @@
+import {render, replace} from "../utils/render";
+import FilmComponent from "../components/film-card";
+import FilmDetailsComponent from "../components/film-details";
+
+export default class MovieController {
+  constructor(container) {
+    this._container = container;
+
+  }
+
+  render(movie) {
+
+  }
+}
